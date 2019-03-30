93598ce287d3fff6f6241f1e949a515527eb3c07
Hafidh Satyanto <hafidhsatyanto@gmail.com>
2019-03-30 12:47:42 -0500

    post-meeting commit

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -17,3 +17,4 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
---
+++ lib/src/widgets/card_menu_picture.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Card_MenuPicture extends StatelessWidget {
+  @override
+  final String title;
+  final 
+
+  Widget build(BuildContext)
+}
