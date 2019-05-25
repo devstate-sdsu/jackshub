e2fdbd0c99fa2b1413a5d5176b9e6f053522be75
Hafidh Satyanto <hafidhsatyanto@Hafidhs-MacBook-Pro.local>
2019-05-25 18:29:23 -0500

    added tabs and sliver app bar

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -3,18 +3,96 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: Text('Jacks Hub'),
-      ),
-      body: Center(
-        child: Text('Hello World!'),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(height: 50.0),
-      ),
+    return new DefaultTabController(
+      length: 3,
+      child: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverOverlapAbsorber(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              child: SliverAppBar(
+                title: const Text('Jacks Hub'),
+                centerTitle: true,
+                pinned: false,
+                floating: true,
+                expandedHeight: 150.0,
+                forceElevated: innerBoxIsScrolled,
+                bottom: TabBar(
+                  tabs: <Widget>[
+                    Tab(icon: new Icon(Icons.event)),
+                    Tab(icon: new Icon(Icons.room_service)),
+                    Tab(icon: new Icon(Icons.fastfood)),
+                  ]
+                ),
+              ),
+            ),
+          ];
+        },
+        body: TabBarView(
+          children: <Widget>[
+            new Container(
+              color: Colors.white,
+            ),
+            new Container(
+              color: Colors.grey,
+            ),
+            new Container(
+              color: Colors.white,
+            )
+          ]
+        )
+
+      )
     );
   }
+
+  // Widget build(BuildContext context) {
+  //   return new DefaultTabController(
+  //     length: 3,
+  //     child: new Scaffold(
+  //       appBar: AppBar(
+  //         backgroundColor: Colors.white,
+  //         title: Text('Jacks Hub'),
+  //       ),
+  //       body: TabBarView(
+  //         children: <Widget>[
+  //           new Container(
+  //             color: Colors.white30,
+  //           ),
+  //           new Container(
+  //             color: Colors.white70,
+  //           ),
+  //           new Container(
+  //             color: Colors.grey,
+  //             child: Image.network(
+  //               'https://picsum.photos/250?image=9',
+  //             )
+  //           ),
+  //         ]
+  //       ),
+  //       bottomNavigationBar: new BottomAppBar(
+  //         color: Colors.white,
+  //         child: new TabBar(
+            
+  //           tabs: <Widget>[
+  //             Tab(icon: new Icon(Icons.event)),
+  //             Tab(icon: new Icon(Icons.room_service)),
+  //             Tab(icon: new Icon(Icons.fastfood)),
+  //           ],
+  //           labelColor: Colors.black,
+  //           unselectedLabelColor: Colors.grey,
+  //           indicatorSize: TabBarIndicatorSize.label,
+  //           indicatorPadding: EdgeInsets.all(0.0),
+  //           indicatorColor: Colors.red,
+  //         )
+  //       ),
+  //     )
+  //   );
+  // }
+
+
+  
 }
 
