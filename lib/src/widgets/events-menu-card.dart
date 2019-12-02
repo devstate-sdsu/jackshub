0bf038791c2e23fee0d209d6741639741d69ccfd
Jin Hang <jinhang@query.ai>
2019-12-01 21:31:11 -0600

    Image with background

--- lib/src/widgets/events-menu-card.dart
+++ lib/src/widgets/events-menu-card.dart
@@ -41,6 +41,13 @@ class EventsMenuCard extends StatelessWidget {
             child: Container(
               margin: EdgeInsets.all(20),
               decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                    image: NetworkImage(
+                      this.img
+                    ),
+                  ),
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(15),
                   boxShadow: [
@@ -55,94 +62,102 @@ class EventsMenuCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  ClipRRect(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15)
-                    ),
-                    child: Image(
-                      image: Image.network(this.img).image,
-                      fit: BoxFit.fitWidth,
-                      height: this.height *.618,
+                  Expanded(
+                    flex: 618,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15)
+                      ),
+                      child: Image(
+                        image: Image.network(this.img).image,
+                        fit: BoxFit.fitHeight,
+                      ),
                     ),
                   ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(15),
-                        bottomRight: Radius.circular(15)
-                    ),
-                    child: Container(
-                      height: this.height * 0.382,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Expanded(
-                                  child: SizedBox(
-                                    width: double.infinity,
-                                    child: FractionallySizedBox(
-                                      widthFactor: 0.89,
-                                      child: Padding(
-                                        padding: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 0.0),
-                                        child: Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: <Widget>[
-                                            Flexible(
-                                              child: AutoSizeText(
-                                                this.name,
-                                                textAlign: TextAlign.left,
-                                                minFontSize: 20,
-                                                style: TextStyle(
-                                                  fontFamily: 'SF Pro',
-                                                  fontWeight: FontWeight.w700,
+                  Expanded(
+                    flex: 382,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(15),
+                          bottomRight: Radius.circular(15)
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        height: this.height * 0.382,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  Expanded(
+                                    child: SizedBox(
+                                      width: double.infinity,
+                                      child: FractionallySizedBox(
+                                        widthFactor: 0.89,
+                                        child: Padding(
+                                          padding: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 0.0),
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              Flexible(
+                                                child: AutoSizeText(
+                                                  this.name,
+                                                  textAlign: TextAlign.left,
+                                                  minFontSize: 20,
+                                                  style: TextStyle(
+                                                    fontFamily: 'SF Pro',
+                                                    fontWeight: FontWeight.w700,
+                                                  ),
+                                                  maxLines: 2,
                                                 ),
-                                                maxLines: 2,
                                               ),
-                                            ),
-                                            AutoSizeText(
-                                              this.summary,
-                                              textAlign: TextAlign.left,
-                                              maxLines: 2,
-                                              minFontSize: 13,
-                                              overflow: TextOverflow.ellipsis,
-                                            ),
-                                          ],
+                                              AutoSizeText(
+                                                this.summary,
+                                                textAlign: TextAlign.left,
+                                                maxLines: 2,
+                                                minFontSize: 13,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ],
+                                          ),
                                         ),
                                       ),
                                     ),
                                   ),
-                                ),
-                                // SizedBox(
-                                //   width: double.infinity,
-                                //   child: FractionallySizedBox(
-                                //     widthFactor: 0.89,
-                                //     child: Padding(
-                                //       padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 2.0),
-                                //       child: AutoSizeText(
-                                //         this.summary,
-                                //         textAlign: TextAlign.left,
-                                //         maxLines: 2,
-                                //         minFontSize: 13,
-                                //         overflow: TextOverflow.ellipsis,
-                                //       ),
-                                //     ),
-                                //   ),
-                                // ),
-                                // Container(
-                                //   child: Padding(
-                                //     padding: EdgeInsets.all(5.0),
-                                //     child: FavoriteWidget(
-                                //       docId: docId,
-                                //     ),
-                                //   ),
-                                // ),
-                              ],
+                                  // SizedBox(
+                                  //   width: double.infinity,
+                                  //   child: FractionallySizedBox(
+                                  //     widthFactor: 0.89,
+                                  //     child: Padding(
+                                  //       padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 2.0),
+                                  //       child: AutoSizeText(
+                                  //         this.summary,
+                                  //         textAlign: TextAlign.left,
+                                  //         maxLines: 2,
+                                  //         minFontSize: 13,
+                                  //         overflow: TextOverflow.ellipsis,
+                                  //       ),
+                                  //     ),
+                                  //   ),
+                                  // ),
+                                  // Container(
+                                  //   child: Padding(
+                                  //     padding: EdgeInsets.all(5.0),
+                                  //     child: FavoriteWidget(
+                                  //       docId: docId,
+                                  //     ),
+                                  //   ),
+                                  // ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
