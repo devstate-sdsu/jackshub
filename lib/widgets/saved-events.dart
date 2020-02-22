21a2776512f11d310a80d81d1fbb01b6c8fe5a07
Oreology <hafidhsatyanto@gmail.com>
2020-02-21 23:06:18 -0600

    added empty text on saved events page

--- lib/widgets/saved-events.dart
+++ lib/widgets/saved-events.dart
@@ -7,6 +7,7 @@ import 'package:jackshub/src/blocs/saved_events/saved_events_bloc.dart';
 import 'package:jackshub/widgets/ColorLoader.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jackshub/src/blocs/saved_events/saved_events_state.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 Widget buildLoadingSavedEvents() {
   return Container(
@@ -22,30 +23,117 @@ class SavedEvents extends StatelessWidget {
     return NotificationListener(
       child: BlocBuilder<SavedEventsBloc, SavedEventsState>(
         builder: (context, state) {
+          print("NOTILISTENER LENGTH: ");
+          print(state.savedEventsInfo.length);
           if (state is SavedEventsInfoLoadedFromLocal) {
-            return ListView.builder(
-              key: PageStorageKey('SavedEvents!Initial'),
-              padding: EdgeInsets.only(
-                bottom: screenHeight * (AppTheme.filterTabsBottomPaddingPercent + AppTheme.filterTabsHeightPercent),
-              ),
-              itemCount: state.savedEventsInfo.length,
-              itemBuilder: (_, index) => EventsScreen.buildEventsListItem(
-                state.savedEventsInfo[index], 
-                true
-              )
-            );
+            if (state.savedEventsInfo.length == 0) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).backgroundColor
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.sentiment_dissatisfied,
+                        color: Theme.of(context).accentColor,
+                        size : 40.0
+                      ),
+                      SizedBox(
+                        height: 15.0
+                      ),
+                      AutoSizeText(
+                        "You have not yet favorited an event!",
+                        maxLines: 2,
+                        maxFontSize: AppTheme.cardDescriptionTextSize.max,
+                        minFontSize: AppTheme.cardDescriptionTextSize.min,
+                        style: Theme.of(context).textTheme.caption
+                      )
+                    ],
+                  )
+                )
+              );
+            } else {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).backgroundColor
+                ),
+                child: ListView.builder(
+                  key: PageStorageKey('SavedEvents!Initial'),
+                  padding: EdgeInsets.only(
+                    bottom: screenHeight * (AppTheme.filterTabsBottomPaddingPercent + AppTheme.filterTabsHeightPercent),
+                  ),
+                  itemCount: state.savedEventsInfo.length,
+                  itemBuilder: (_, index) => EventsScreen.buildEventsListItem(
+                    state.savedEventsInfo[index], 
+                    true
+                  )
+                )
+              );
+              // return ListView.builder(
+              //   key: PageStorageKey('SavedEvents!Initial'),
+              //   padding: EdgeInsets.only(
+              //     bottom: screenHeight * (AppTheme.filterTabsBottomPaddingPercent + AppTheme.filterTabsHeightPercent),
+              //   ),
+              //   itemCount: state.savedEventsInfo.length,
+              //   itemBuilder: (_, index) => EventsScreen.buildEventsListItem(
+              //     state.savedEventsInfo[index], 
+              //     true
+              //   )
+              // );
+            }
           } else if (state is InSavedEventsScreen) {
-            return ListView.builder(
-              key: PageStorageKey('SavedEvents!AddingOrDeleting'),
-              padding: EdgeInsets.only(
-                bottom: screenHeight * (AppTheme.filterTabsBottomPaddingPercent + AppTheme.filterTabsHeightPercent),
-              ),
-              itemCount: state.savedEventsInfo.length,
-              itemBuilder: (_, index) => EventsScreen.buildEventsListItem(
-                state.savedEventsInfo[index], 
-                !state.toDeleteMap.containsKey(state.savedEventsInfo[index].documentId)
-              )
-            );
+            print("SAVED EVENTS SCREEN LENGTH: ");
+            print(state.savedEventsInfo.length);
+            if (state.savedEventsInfo.length == 0) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).backgroundColor
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.sentiment_dissatisfied,
+                        color: Theme.of(context).accentColor,
+                        size : 40.0
+                      ),
+                      SizedBox(
+                        height: 15.0
+                      ),
+                      AutoSizeText(
+                        "You have not yet favorited an event!",
+                        maxLines: 2,
+                        maxFontSize: AppTheme.cardDescriptionTextSize.max,
+                        minFontSize: AppTheme.cardDescriptionTextSize.min,
+                        style: Theme.of(context).textTheme.caption
+                      )
+                    ],
+                  )
+                )
+              );
+            } else {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).backgroundColor
+                ),
+                child: ListView.builder(
+                  key: PageStorageKey('SavedEvents!AddingOrDeleting'),
+                  padding: EdgeInsets.only(
+                    bottom: screenHeight * (AppTheme.filterTabsBottomPaddingPercent + AppTheme.filterTabsHeightPercent),
+                  ),
+                  itemCount: state.savedEventsInfo.length,
+                  itemBuilder: (_, index) => EventsScreen.buildEventsListItem(
+                    state.savedEventsInfo[index], 
+                    !state.toDeleteMap.containsKey(state.savedEventsInfo[index].documentId)
+                  )
+                )
+              );
+            }
           }
           return buildLoadingSavedEvents();
         },
